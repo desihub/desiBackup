8a4b9ab3583fc90fcb94cae01bed273e25ee9bf0
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-04-23 12:19:33 -0400

    add options to script

--- bin/desiBackup.sh
+++ bin/desiBackup.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
-missing_from_hpss ${DESIBACKUP}/desi.json $1
+function usage() {
+    local execName=$(basename $0)
+    (
+    echo "${execName} [-h] [-v] DIR"
+    echo ""
+    echo "Install desiUtil on a bare system."
+    echo ""
+    echo "   DIR = Top-level directory to examine for backups."
+    echo "    -h = Print this message and exit."
+    echo "    -v = Verbose mode. Print lots of extra information."
+    ) >&2
+}
+#
+# Get options
+#
+verbose=''
+while getopts hv argname; do
+    case ${argname} in
+        h) usage; exit 0 ;;
+        v) verbose='-v' ;;
+        *) usage; exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+#
+#
+#
+if [[ $# < 1 ]]; then
+    echo "You must specify a top-level directory!" >&2
+    exit 1
+fi
+#
+#
+#
+missing_from_hpss ${verbose} ${DESIBACKUP}/desi.json $1
