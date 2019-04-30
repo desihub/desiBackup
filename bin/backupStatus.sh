50fd477f1719439a26cc50fd9bd25036ebd6edf9
Benjamin Alan Weaver <baweaver@lbl.gov>
2019-04-30 11:52:16 -0700

    add specific comments

--- bin/backupStatus.sh
+++ bin/backupStatus.sh
@@ -42,6 +42,7 @@ function row() {
     local s=$2
     local n=$3
     local c=$4
+    local o=$5
     local space='                            '
     local tcls=''
     if [[ "${s}" == "COMPLETE" ]]; then
@@ -55,25 +56,25 @@ function row() {
     elif [[ "${s}" == "NO CONFIGURATION" ]]; then
         tcls='danger'
     else
-        echo 'Unknown status!'
+        echo 'Unknown status!' >&2
         return
     fi
-    echo "${space}<tr>"
-    echo "${space}    <td>${d}/</td>"
-    echo "${space}    <td class=\"table-${tcls}\"><strong>${s}</strong></td>"
+    echo "${space}<tr>" >> ${o}
+    echo "${space}    <td>${d}/</td>" >> ${o}
+    echo "${space}    <td class=\"table-${tcls}\"><strong>${s}</strong></td>" >> ${o}
     if [[ "${n}" == "False" ]]; then
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">CSV</a></td>"
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">TXT</a></td>"
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">JSON</a></td>"
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">LOG</a></td>"
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">CSV</a></td>" >> ${o}
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">TXT</a></td>" >> ${o}
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">JSON</a></td>" >> ${o}
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-light\" role=\"button\" href=\"#\" title=\"Status not run.\">LOG</a></td>" >> ${o}
     else
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"disk_files_${d}.csv\" title=\"disk_files_${d}.csv\">CSV</a></td>"
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"hpss_files_${d}.txt\" title=\"hpss_files_${d}.txt\">TXT</a></td>"
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"missing_files_${d}.json\" title=\"missing_files_${d}.json\">JSON</a></td>"
-        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"missing_files_${d}.log\" title=\"missing_files_${d}.log\">LOG</a></td>"
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"disk_files_${d}.csv\" title=\"disk_files_${d}.csv\">CSV</a></td>" >> ${o}
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"hpss_files_${d}.txt\" title=\"hpss_files_${d}.txt\">TXT</a></td>" >> ${o}
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"missing_files_${d}.json\" title=\"missing_files_${d}.json\">JSON</a></td>" >> ${o}
+        echo "${space}    <td><a class=\"btn btn-sm btn-outline-primary\" role=\"button\" href=\"missing_files_${d}.log\" title=\"missing_files_${d}.log\">LOG</a></td>" >> ${o}
     fi
-    echo "${space}    <td>${c}</td>"
-    echo "${space}</tr>"
+    echo "${space}    <td>${c}</td>" >> ${o}
+    echo "${space}</tr>" >> ${o}
 }
 #
 # Get options.
@@ -105,36 +106,35 @@ if [[ ! -d ${cacheDir} ]]; then
     chmod o+rx ${cacheDir}
 fi
 #
+# Start the HTML table.
+#
+[[ -f ${cacheDir}/index.html ]] && /bin/rm -f ${cacheDir}/index.html
+timestamp=$(date '+%Y-%m-%d %H:%M:%S %Z')
+cutLine=$(grep --line-number "INSERT CONTENT HERE" ${DESIBACKUP}/etc/backupStatus.html | cut -d: -f1)
+head -$((cutLine - 1)) ${DESIBACKUP}/etc/backupStatus.html | \
+    sed "s%<caption>Last Update: DATE</caption>%<caption>Last Update: ${timestamp}</caption>" > ${cacheDir}/index.html
+#
 # Check all sections in desi.json.
 #
 sections=$(grep -E '^    "[^"]+":\{' ${DESIBACKUP}/etc/desi.json | \
            sed -r 's/^    "([^"]+)":\{/\1/' | \
            grep -v config)
-# cmx: not configured for backup
-# cosmosim: not configured for backup
-# datachallenge:  AUTOFILL
-# engineering: not configured for backup
-# external: empty, not configured for backup
-# metadata: not configured for backup
-# mocks: AUTOFILL
-# protodesi: AUTOFILL (should always be done!)
-# release: empty, not configured for backup
-# science: mostly empty, not configured for backup
-# software: deliberately excluded from backup.
-# spectro: AUTOFILL
-# survey: not configured for backup
-# target: AUTOFILL
-# users: deliberately excluded from backup.
-# www: not configured for backup
+comments=$(cat <<COMMENTS
+datachallenge:<code>quicklook</code> is missing.
+mocks:<code>lya_forest</code> is missing.
+spectro:Only partially configured for backup.
+target:Only <code>cmx_files</code> is configured for backup.
+COMMENTS
+)
 for d in ${sections}; do
     if [[ "${d}" == "external" ]]; then
-        row ${d} COMPLETE False 'Deprecated, empty directory.'
+        row ${d} COMPLETE False 'Deprecated, empty directory.' ${cacheDir}/index.html
     elif [[ "${d}" == "release" ]]; then
-        row ${d} 'NO DATA' False 'Empty directory, no results yet!'
+        row ${d} 'NO DATA' False 'Empty directory, no results yet!' ${cacheDir}/index.html
     elif [[ "${d}" == "software" ]]; then
-        row ${d} 'NO BACKUP' False 'Most DESI software is stored elsewhere, and the ultimate backups are the various git and svn repositories.'
+        row ${d} 'NO BACKUP' False 'Most DESI software is stored elsewhere, and the ultimate backups are the various git and svn repositories.' ${cacheDir}/index.html
     elif [[ "${d}" == "users" ]]; then
-        row ${d} 'NO BACKUP' False 'The default policy is for the users directory to serve as long-term scratch space, so it is not backed up.'
+        row ${d} 'NO BACKUP' False 'The default policy is for the users directory to serve as long-term scratch space, so it is not backed up.' ${cacheDir}/index.html
     else
         [[ -f ${cacheDir}/missing_files_${d}.log ]] && /bin/rm -f ${cacheDir}/missing_files_${d}.log
         [[ -n "${verbose}" ]] && echo missing_from_hpss ${verbose} -D -H -c ${cacheDir} ${DESIBACKUP}/etc/desi.json ${d}
@@ -142,21 +142,24 @@ for d in ${sections}; do
         hpss_files=$(<${cacheDir}/hpss_files_${d}.txt)
         missing_files=$(<${cacheDir}/missing_files_${d}.json)
         missing_log=$(<${cacheDir}/missing_files_${d}.log)
+        comment=$(grep "${d}:" <<<"${comments}" | cut -d: -f2)
         if [[ -z "${hpss_files}" && "${missing_files}" == "{}" ]]; then
-            row ${d} 'NO CONFIGURATION' True 'Not configured for backup.'
-        elif [[ -n "${hpss_files}" && "${missing_files}" == "{}" ]]; then
-            row ${d} COMPLETE True 'No missing files found.'
+            [[ -z "${comment}" ]] && comment='Not configured for backup.'
+            row ${d} 'NO CONFIGURATION' True "${comment}" ${cacheDir}/index.html
+        elif [[ -n "${hpss_files}" && -z "${missing_log}" && "${missing_files}" == "{}" ]]; then
+            [[ -z "${comment}" ]] && comment='No missing files found.'
+            row ${d} COMPLETE True "${comment}" ${cacheDir}/index.html
         else
-            row ${d} 'IN PROGRESS' True 'In progress.'
+            [[ -z "${comment}" ]] && comment='In progress.'
+            row ${d} 'IN PROGRESS' True "${comment}" ${cacheDir}/index.html
         fi
     fi
 done
 #
+# Finish the HTML table.
+#
+tail -n +$((cutLine - 1)) ${DESIBACKUP}/etc/backupStatus.html >> ${cacheDir}/index.html
+#
 # Make sure the files are readable.
 #
 chmod o+r ${cacheDir}/*
-#
-#
-#
-timestamp=$(date '+%Y-%m-%d %H:%M:%S %Z')
-sed "s%<caption>Last Update: DATE</caption>%<caption>Last Update: ${timestamp}</caption>" ${DESIBACKUP}/etc/backupStatus.html > ${cacheDir}/index.html
